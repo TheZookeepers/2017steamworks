8a80e05717cdaa55c4be06795de260dc6668f7e6
PorkyPig <PorkyPig@LAPTOP-VDC0L2M7>
2017-01-09 17:08:39 -0500

    Added code for basic driving.

--- src/org/usfirst/frc/team3603/robot/Robot.java
+++ src/org/usfirst/frc/team3603/robot/Robot.java
@@ -6,15 +6,56 @@
  ****************************************/
 package org.usfirst.frc.team3603.robot;
 
+import edu.wpi.first.wpilibj.ADXL362;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
 
 public class Robot extends IterativeRobot {
+	Joystick joy1 = new Joystick(0);
+	Joystick joy2 = new Joystick(1);
+	
+    Victor backLeft = new Victor(0);
+    Victor backRight = new Victor(1);
+    Victor frontLeft = new Victor(2);
+    Victor frontRight = new Victor(3);
     
+    RobotDrive mainDrive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+    
+    ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+    ADXL362 accel = new ADXL362(Range.k8G);
+    Timer timer = new Timer();
+    
+    public void robotInit() {
+    	gyro.calibrate();
+    	gyro.reset();
+    }
 	public void autonomousInit() {
     }
     public void autonomousPeriodic() {
     }
     public void teleopPeriodic() {
+    	while(isOperatorControl() && isEnabled()) {
+    		if(joy1.getRawButton(1) || joy1.getRawButton(2) || joy1.getRawButton(3) || joy1.getRawButton(4) || joy1.getRawButton(5) || joy1.getRawButton(6) || joy1.getRawButton(7) || joy1.getRawButton(8) || joy1.getRawButton(9) || joy1.getRawButton(10) ||  joy2.getRawButton(1) || joy2.getRawButton(2) || joy2.getRawButton(3) || joy2.getRawButton(4) || joy2.getRawButton(5) || joy2.getRawButton(6) || joy2.getRawButton(7) || joy2.getRawButton(8) || joy2.getRawButton(9) || joy2.getRawButton(10) || joy1.getRawAxis(0) >= 0.05 || joy1.getRawAxis(1) >= 0.05 || joy1.getRawAxis(2) >= 0.05 || joy1.getRawAxis(3) >= 0.05 || joy1.getRawAxis(4) >= 0.05 || joy1.getRawAxis(5) >= 0.05 || joy1.getRawAxis(6) >= 0.05 || joy2.getRawAxis(0) >= 0.05 || joy2.getRawAxis(1) >= 0.05 || joy2.getRawAxis(2) >= 0.05 || joy2.getRawAxis(3) >= 0.05 || joy2.getRawAxis(4) >= 0.05 || joy2.getRawAxis(5) >= 0.05 || joy2.getRawAxis(6) >= 0.05 || joy1.getRawAxis(0) <= -0.05 || joy1.getRawAxis(1) <= -0.05 || joy1.getRawAxis(2) <= -0.05 || joy1.getRawAxis(3) <= -0.05 || joy1.getRawAxis(4) <= -0.05 || joy1.getRawAxis(5) <= -0.05 || joy1.getRawAxis(6) <= -0.05 || joy2.getRawAxis(0) <= -0.05 || joy2.getRawAxis(1) <= -0.05 || joy2.getRawAxis(2) <= -0.05 || joy2.getRawAxis(3) <= -0.05 || joy2.getRawAxis(4) <= -0.05 || joy2.getRawAxis(5) <= -0.05 || joy2.getRawAxis(6) <= -0.05) {
+    			/***********************
+	    		 *** DRIVER CONTROLS ***
+	    		 ***********************/
+	    		
+	    		double x = Math.pow(joy1.getRawAxis(0), 3);
+	    		double y = Math.pow(joy1.getRawAxis(1), 3);
+	    		double rot = Math.pow(joy1.getTwist(), 3);
+	    		
+	    		if(Math.abs(x)>=0.1 || Math.abs(y)>=0.1 || Math.abs(rot)>=0.1) {
+	    			mainDrive.mecanumDrive_Cartesian(x, y, rot, gyro.getAngle());
+	    		}
+    		} else {
+    			mainDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
+    		}
+    	}
     }
     public void testPeriodic() {
     }
