7e1f163e5f9b4c97d6968225e956c14c4cf61e5e
sadle <sadle@Courser-7415K9>
2017-01-21 15:48:35 -0500

    First code

--- src/org/usfirst/frc/team3603/robot/Robot.java
+++ src/org/usfirst/frc/team3603/robot/Robot.java
@@ -19,208 +19,28 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
-	final String defaultAuto = "Default";
-	final String redAuton = "redAuton";
-	final String blueAuton = "blueAuton";
-	final static int visAll = 20;
-	String autoSelected;
-	SendableChooser<String> chooser = new SendableChooser<>();
+	Victor frontLeft = new Victor(0);
+	Victor frontRight = new Victor(1);
+	Victor backLeft = new Victor(2);
+	Victor backRight = new Victor(3);
+	RobotDrive mainDrive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
 	
-	Joystick joy1 = new Joystick(0);
-	Joystick joy2 = new Joystick(1);
-	
-    Victor backLeft = new Victor(1);//
-    Victor backRight = new Victor(2);//
-    Victor frontLeft = new Victor(3);//
-    Victor frontRight = new Victor(4);//
-    Victor shoot = new Victor(5);
-    
-    RobotDrive mainDrive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
-    
-    ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-    ADXL362 accel = new ADXL362(Range.k8G);
-    Timer timer = new Timer();
-    Encoder enc = new Encoder(0, 1, true, Encoder.EncodingType.k4X);
-    
-    int x = 0;
-    
-    Vision2017 vision = new Vision2017(0);
-    
-	public void robotInit() {
-    	frontRight.setInverted(true);
-    	backRight.setInverted(true);
-    	gyro.calibrate();
-    	gyro.reset();
-    	chooser.addDefault("Default Auto", defaultAuto);
-		chooser.addObject("Red Autonomous Code", redAuton);
-		chooser.addObject("Blue Autonomous Code", blueAuton);
-		SmartDashboard.putData("Auto choices", chooser);
-		
-		final double Encoder_Distance = 5.0/12.0*Math.PI/270.0;
-		
-    	enc.setDistancePerPulse(Encoder_Distance);
-    	enc.setSamplesToAverage(7);
+	Joystick joystick = new Joystick(0);
+	Joystick xbox = new Joystick(1);
+    public void robotInit() {
+    	
     }
 	public void autonomousInit() {
-		autoSelected = chooser.getSelected();
     }
     public void autonomousPeriodic() {
-    	timer.start();
-    	switch (autoSelected) {
-		case defaultAuto:
-			DefaultAuto();
-			break;
-
-		case redAuton:
-			RedAuton();
-			break;
-		
-		case blueAuton:
-			BlueAuton();
-			break;
-    	}
     }
     
 	public void teleopPeriodic() {
-    	while(isOperatorControl() && isEnabled()) {
-    		if(joy1.getRawButton(1) || joy1.getRawButton(2) || joy1.getRawButton(3) || joy1.getRawButton(4) || joy1.getRawButton(5) || joy1.getRawButton(6) || joy1.getRawButton(7) || joy1.getRawButton(8) || joy1.getRawButton(9) || joy1.getRawButton(10) ||  joy2.getRawButton(1) || joy2.getRawButton(2) || joy2.getRawButton(3) || joy2.getRawButton(4) || joy2.getRawButton(5) || joy2.getRawButton(6) || joy2.getRawButton(7) || joy2.getRawButton(8) || joy2.getRawButton(9) || joy2.getRawButton(10) || joy1.getRawAxis(0) >= 0.05 || joy1.getRawAxis(1) >= 0.05 || joy1.getRawAxis(2) >= 0.05 || joy1.getRawAxis(3) >= 0.05 || joy1.getRawAxis(4) >= 0.05 || joy1.getRawAxis(5) >= 0.05 || joy1.getRawAxis(6) >= 0.05 || joy2.getRawAxis(0) >= 0.05 || joy2.getRawAxis(1) >= 0.05 || joy2.getRawAxis(2) >= 0.05 || joy2.getRawAxis(3) >= 0.05 || joy2.getRawAxis(4) >= 0.05 || joy2.getRawAxis(5) >= 0.05 || joy2.getRawAxis(6) >= 0.05 || joy1.getRawAxis(0) <= -0.05 || joy1.getRawAxis(1) <= -0.05 || joy1.getRawAxis(2) <= -0.05 || joy1.getRawAxis(3) <= -0.05 || joy1.getRawAxis(4) <= -0.05 || joy1.getRawAxis(5) <= -0.05 || joy1.getRawAxis(6) <= -0.05 || joy2.getRawAxis(0) <= -0.05 || joy2.getRawAxis(1) <= -0.05 || joy2.getRawAxis(2) <= -0.05 || joy2.getRawAxis(3) <= -0.05 || joy2.getRawAxis(4) <= -0.05 || joy2.getRawAxis(5) <= -0.05 || joy2.getRawAxis(6) <= -0.05) {
-    			/***********************
-	    		 *** DRIVER CONTROLS ***
-	    		 ***********************/
-    			
-	    		final double CENTER_IMAGE = vision.GetCameraWidth()/2;
-	    		double x = Math.pow(joy1.getRawAxis(0), 3);
-	    		double y = Math.pow(joy1.getRawAxis(1), 3);
-	    		double rot = Math.pow(joy1.getTwist(), 3)/2;
-	    		
-	    		if(Math.abs(x)>=0.1 || Math.abs(y)>=0.1 || Math.abs(rot)>=0.1 && joy1.getRawButton(2)) {
-	    			mainDrive.mecanumDrive_Cartesian(x, y, rot, gyro.getAngle());
-	    		} else if(Math.abs(x)>=0.1 || Math.abs(y)>=0.1 || Math.abs(rot)>=0.1) {
-	    			mainDrive.mecanumDrive_Cartesian(x, y, rot, 0);
-	    		} else {
-	    			
-	    		}
-	    		if(enc.getDistance() < 2.0) {
-	    			mainDrive.mecanumDrive_Cartesian(0, -0.5, 0, 0);
-	    		}
-	    		
-	    		while(joy1.getPOV()!=-1 && !joy1.getRawButton(1)) {
-	    			int pov = joy1.getPOV();
-	    			switch(pov) {
-	    			case -1:
-	    				break;
-	    			case 0:
-	    				mainDrive.mecanumDrive_Cartesian(0, -.75, 0, 0);
-	    				break;
-	    			case 45:
-	    				mainDrive.mecanumDrive_Cartesian(-.75, -.75, 0, 0);
-	    				break;
-	    			case 90:
-	    				mainDrive.mecanumDrive_Cartesian(-.75, 0, 0, 0);
-	    				break;
-	    			case 135:
-	    				mainDrive.mecanumDrive_Cartesian(-.75, .75, 0, 0);
-	    				break;
-	    			case 180:
-	    				mainDrive.mecanumDrive_Cartesian(0, .75, 0, 0);
-	    				break;
-	    			case 225:
-	    				mainDrive.mecanumDrive_Cartesian(.75, .75, 0, 0);
-	    				break;
-	    			case 270:
-	    				mainDrive.mecanumDrive_Cartesian(.75, 0, 0, 0);
-	    				break;
-	    			case 315:
-	    				mainDrive.mecanumDrive_Cartesian(.75, -.75, 0, 0);
-	    				break;
-	    			}
-	    			read();
-	    		}
-	    		
-	    		while(joy1.getRawButton(1)) {
-	    			mainDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
-	    			read();
-	    		}
-	    		boolean gear = false;
-	    		
-	    		if(joy1.getRawButton(4)) {
-	    			gyro.reset();
-	    			enc.reset();
-	    		}
-	    		
-	    		while(joy1.getRawButton(3)) {
-		    		while(joy1.getRawButton(3) && gear == false) {
-		    			if(vision.centerGear()> CENTER_IMAGE + visAll) {
-		    				mainDrive.mecanumDrive_Cartesian(0, 0, -.28, 0);
-		    			} else if(vision.centerGear()< CENTER_IMAGE - visAll) {
-		    				mainDrive.mecanumDrive_Cartesian(0, 0, .28, 0);
-		    			} else if(vision.centerGear() > CENTER_IMAGE - visAll && vision.centerGear() < CENTER_IMAGE + visAll) {
-		    				mainDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
-		    				gear = true;
-		    			}
-		    			read();
-		    		}
-		    		read();
-	    		}
-	    		
-    		} else {
-    			mainDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
-    		}
-    		try {
-    			read();
-				Thread.sleep(25);
-			} catch (InterruptedException e) {
-			}
-    	}
+		while(isEnabled() && isOperatorControl()){
+			double x = joystick.getRawAxis(0);
+			double y = joystick.getRawAxis(1);
+		}
     }
     public void testPeriodic() {
     }
-    
-    void read() {
-    	SmartDashboard.putNumber("Center X", vision.centerGear());
-		SmartDashboard.putNumber("Center Y", vision.GetContour1CenterY());
-		SmartDashboard.putNumber("Gyro angle", gyro.getAngle());
-		SmartDashboard.putNumber("Encoder", enc.getDistance());
-    }
-    
-    private void DefaultAuto() {
-    	timer.reset();
-    	gyro.reset();
-    	while(isAutonomous() && isEnabled() && timer.get() < 6.0) {
-    		mainDrive.mecanumDrive_Cartesian(0, -0.2, 0.2, gyro.getAngle());
-    		read();
-    	}
-	}
-    
-    private void RedAuton() {
-    	timer.reset();
-    	gyro.reset();
-    	
-    	while(isAutonomous() && isEnabled()) {
-    		boolean end = false;
-    		while(timer.get() < 3 && end == false) {
-    			if(vision.centerGear()> vision.GetCameraWidth() + visAll) {
-    				mainDrive.mecanumDrive_Cartesian(0, 0, -.28, 0);
-    			} else if(vision.centerGear()< vision.GetCameraWidth() - visAll) {
-    				mainDrive.mecanumDrive_Cartesian(0, 0, .28, 0);
-    			} else {
-    				mainDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
-    				end = true;
-    			}
-    			read();
-    		}
-    		mainDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
-    		read();
-    	}
-    	
-    }
-    
-    private void BlueAuton() {
-    	while(isAutonomous() && isEnabled()) {
-    		if(enc.getDistance() < 5.0) {
-    			mainDrive.mecanumDrive_Cartesian(0, -0.5, 0, 0);
-    		}
-    	}
-    }
-    
 }
